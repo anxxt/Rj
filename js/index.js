d5297fd96258c9f3be7de7082e54a56bd9fa9d87
anxxt <antonio.garcia-garcia@outlook.com>
2024-02-20 14:39:19 +0100

    add js, show the current day exercises

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const putPhoto = (day, img) => {
+  if (day >= 1 && day <= 5) {
+    img.src = `img/day${day}.avif`
+    img.alt = `Ejercicios del día ${day}`
+  } else {
+    img.remove()
+    const newH1 = document.createElement("h1")
+    newH1.innerText = "No hay ejercicios para hoy"
+    document.body.appendChild(newH1)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const day = new Date().getDay()
+  const img = document.querySelector("img")
+
+  putPhoto(day, img)
+})
